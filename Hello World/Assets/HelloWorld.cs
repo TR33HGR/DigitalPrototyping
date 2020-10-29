5a7784e80b58a098c30d8826d88c6d33dabfeb30
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-10-29 20:34:36 +0000

    Print Hello World on Update instead of on Start

--- Hello World/Assets/HelloWorld.cs
+++ Hello World/Assets/HelloWorld.cs
@@ -5,11 +5,11 @@ public class HelloWorld : MonoBehaviour
 {
   // Start is called before the first frame update
   void Start()
-  {
-    print("Hello World");
-  }
+  {}
 
   // Update is called once per frame
   void Update()
-  {}
+  {
+    print("Hello World");
+  }
 }
