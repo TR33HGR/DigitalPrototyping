45eea2a6b25a8e5487d8de8a1a7d5f4d49bc0b63
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-10 18:51:51 +0000

    An infinite loop

--- LoopsExample/Assets/Loops.cs
+++ LoopsExample/Assets/Loops.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Loops : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class Loops : MonoBehaviour {
+    void Start() {
+        while(true) {
+            print("Loop");
+        }
     }
 }
