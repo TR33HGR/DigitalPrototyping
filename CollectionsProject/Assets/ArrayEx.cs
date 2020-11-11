23edb08839962e90cb0f10fb034c8c004dc0af40
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-11 20:35:57 +0000

    Array example

--- CollectionsProject/Assets/ArrayEx.cs
+++ CollectionsProject/Assets/ArrayEx.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class ArrayEx : MonoBehaviour
 {
+    public string[] sArray;
     // Start is called before the first frame update
     void Start()
     {
+        sArray = new string[10];
+        sArray[0] = "These";
+        sArray[1] = "are";
+        sArray[2] = "some";
+        sArray[3] = "words";
+
+        print("The length of sArray is: " + sArray.Length);
+
+        string str = "";
+        foreach(string sTemp in sArray) {
+            str += "|" + sTemp;
+        }
+        print(str);
         
     }
 
