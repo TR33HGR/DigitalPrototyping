17b69370b7326276b8d47a007eede659214ad927
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-11 20:25:44 +0000

    List example

--- CollectionsProject/Assets/ListEx.cs
+++ CollectionsProject/Assets/ListEx.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class ListEx : MonoBehaviour
 {
+    public List<string> sList;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sList = new List<string>();
+        sList.Add("Experience");
+        sList.Add("is");
+        sList.Add("what");
+        sList.Add("you");
+        sList.Add("get");
+        sList.Add("when");
+        sList.Add("you");
+        sList.Add("didn't");
+        sList.Add("get");
+        sList.Add("what");
+        sList.Add("you");
+        sList.Add("wanted.");
+
+        print("sList Count = " + sList.Count);
+        print("The 0th element is : " + sList[0]);
+        print("The 1st element is : " + sList[1]);
+        print("The 3rd element is : " + sList[3]);
+        print("The 8th element is : " + sList[8]);
+
+        string str = "";
+        foreach(string sTemp in sList) {
+            str += sTemp + " ";
+        }
+        print(str);
     }
 
     // Update is called once per frame
