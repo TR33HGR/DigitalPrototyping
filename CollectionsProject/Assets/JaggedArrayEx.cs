6240269d2535a88a5ca06654d0b86170ad0a3840
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-11 21:13:00 +0000

    JaggedArray example

---
+++ CollectionsProject/Assets/JaggedArrayEx.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaggedArrayEx : MonoBehaviour
+{
+    public string[][] jArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jArray = new string[4][];
+        jArray[0] = new string[4];
+        jArray[0][0] = "A";
+        jArray[0][1] = "B";
+        jArray[0][2] = "C";
+        jArray[0][3] = "D";
+
+        jArray[1] = new string[]{"E", "F", "G"};
+        jArray[2] = new string[]{"H", "I"};
+
+        jArray[3] = new string[4];
+        jArray[3][0] = "J";
+        jArray[3][3] = "K";
+
+        print("The Length of jArray is: " + jArray.Length);
+        print("The Length of jArray[1] is: " + jArray[1].Length);
+
+        string str = "";
+        foreach(string[] sArray in jArray) {
+            foreach(string sTemp in sArray) {
+                if(sTemp != null) {
+                    str += " | " + sTemp;
+                } else {
+                    str += " | ";
+                }
+            }
+            str += " | \n";
+        }
+        print(str);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
