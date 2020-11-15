e2371059a292480c0801a994c1143a97a689cf07
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-15 15:08:26 +0000

    Duplicate CubeSpawner form HelloWorld Project

---
+++ HelloWorld/Assets/CubeSpawner1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+  public GameObject cubePrefabVar;
+
+  // Start is called before the first frame update
+  void Start()
+  {}
+
+  // Update is called once per frame
+  void Update()
+  {
+    Instantiate(cubePrefabVar);
+  }
+}
