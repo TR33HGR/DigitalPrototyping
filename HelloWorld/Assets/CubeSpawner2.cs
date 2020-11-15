6d7036bd15de724122fb6b5604874671d87722c2
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2020-11-15 16:03:58 +0000

    Add example code for using the debugger

--- HelloWorld/Assets/CubeSpawner2.cs
+++ HelloWorld/Assets/CubeSpawner2.cs
@@ -12,6 +12,19 @@ public class CubeSpawner2 : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    SpellItOut();
     Instantiate(cubePrefabVar);
+  }
+
+  public void SpellItOut()
+  {
+    string sA = "Hello World!";
+    string sB = "";
+
+    for(int i = 0; i < sA.Length; i++) {
+      sB += sA[i];
+    }
+
+    print(sB);
   }
 }
